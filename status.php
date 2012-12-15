8d58f8c2c47da10b38a32e9430f07bf748a75b18
Edgar Henderson <edgarhenderson@gmail.com>
2012-12-15 01:30:08 -0500

    Added Twilio text messaging

--- status.php
+++ status.php
@@ -2,6 +2,8 @@
 
 	session_start();
 	include 'dbstuff.php';
+	include 'Services/Twilio.php';
+	include 'TxtMsgSecure.php';
 	mysql_select_db($dbuser,$con);
 	if (!$con) { 
 		die(' Could no connect');
@@ -24,14 +26,21 @@
 		{
 			$query3 = mysql_query("SELECT * FROM patient WHERE IDPATIENT ='$patient'",$con) or die(mysql_error());
 			$patname = mysql_result($query3,0,3) . " " . mysql_result($query3,0,4);
+		
+			// Step 3: instantiate a new Twilio Rest Client
+			$client = new Services_Twilio($AccountSid, $AuthToken);
+			
+			
 			while ($row = mysql_fetch_array($query, MYSQL_ASSOC))
 			{
 				$userid = $row["UserIDPERM"];
 				$query2 = mysql_query("SELECT * FROM users WHERE IDUSER = '$userid'",$con) or die(mysql_error());
 				$message = $_SESSION['name'] . " posted about " . $patname . "-" . $status;
-				$to = mysql_result($query2,0,3) . "@" . mysql_result($query2,0,4);				
-				$headers = 'From: info@CareAndTell.com';								
-				$maxmessage = 120;
+				$to = mysql_result($query2,0,3);				
+						$headers = 'From: info@CareAndTell.com' . "\r\n" .
+							'Reply-To: info@CareAndTell.com' . "\r\n" .
+							'X-Mailer: PHP/' . phpversion();							
+				$maxmessage = 160;
 				$emailtimes = 0;
 				if (strlen($message) > $maxmessage)
 				{
@@ -41,25 +50,57 @@
 					$chunkmessage = str_split($message,$maxmessage);					
 					while ($i <= $emailtimes)
 					{
-						mail($to, $subject, $chunkmessage[$i], $headers);
+				        $sms = $client->account->sms_messages->create("716-261-3316",$to,$chunkmessage[$i]);
+
+//						mail($to, "update", $chunkmessage[$i], $headers);
 						$i++;
 					}
 				}	
 				else
 				{
-					mail($to, $subject, $message, $headers);
+					$sms = $client->account->sms_messages->create($TwilioNumber,$to,$message);
+//					mail("edgarhenderson@gmail.com", "update", $message, $headers);
 //					echo $to . "<br />" . $message . "<br />" . $headers . "<br />";
 				}			
 				
 			}
 		}
 		
-		
+
 		
 		mysql_close($con);	
 		header("Location:mainview.php");
 	}
-	
-	
+
+ 
+ 
+    // Step 4: make an array of people we know, to send them a message. 
+    // Feel free to change/add your own phone number and name here.
+//    $people = array(
+//        "+14158675309" => "Curious George",
+//        "+14158675310" => "Boots",
+//        "+14158675311" => "Virgil",
+//    );
+ 
+    // Step 5: Loop over all our friends. $number is a phone number above, and 
+    // $name is the name next to it
+//    foreach ($people as $number => $name) {
+ 
+//        $sms = $client->account->sms_messages->create("716-261-3316",$number,"Hey" . $name . ", Monkey Party at 6PM. Bring Bananas!");
+ 
+        // Step 6: Change the 'From' number below to be a valid Twilio number 
+        // that you've purchased, or the (deprecated) Sandbox number
+             
+ 
+            // the number we are sending to - Any phone number
+            
+ 
+            // the sms body
+            
+        
+ 
+        // Display a confirmation message on the screen
+ //       echo "Sent message to $name";
+ //   }	
 ?>
 
